89674d5dad67f9d8b0fea9217d44b65e5a284858
tbisanz <tobias.bisanz@phys.uni-goettingen.de>
2017-07-03 11:33:20 +0200

    EUTelescope wont build with -no-undefined

--- cmakemodules/ilcsoft_default_cxx_flags.cmake
+++ cmakemodules/ilcsoft_default_cxx_flags.cmake
@@ -6,9 +6,9 @@ INCLUDE(CheckCXXCompilerFlag)
 
 SET(COMPILER_FLAGS -Wall -Wextra -Wshadow -Weffc++ -pedantic -Wno-long-long -Wuninitialized )
 
-IF( "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" )
- LIST( APPEND COMPILER_FLAGS -Wl,-no-undefined )
-ENDIF()
+#IF( "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" )
+# LIST( APPEND COMPILER_FLAGS -Wl,-no-undefined )
+#ENDIF()
 
 
 MESSAGE( STATUS "FLAGS ${COMPILER_FLAGS}" )
